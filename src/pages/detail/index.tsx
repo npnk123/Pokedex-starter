3c57a31dadffa473e512a36ee2f5e50532de3bc3
Natthaphon <npnk123@gmail.com>
2024-10-08 15:52:08 +0700

    Add Back Button in detail page

--- src/pages/detail/index.tsx
+++ src/pages/detail/index.tsx
@@ -2,6 +2,7 @@ import { pokemonDetailServices } from "@/services";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { IPokemonDetailResponse } from "@/interface/pokemonDetail";
+import { Link } from "react-router-dom";
 
 type pokemonType = {
   data: IPokemonDetailResponse | undefined;
@@ -48,49 +49,57 @@ const DetailPage = () => {
 
   return (
     <div className="w-[90%] m-[auto] max-w-[500px]">
+      <Link to={`/`}>
+        <div className="text-center p-4">
+          <button
+            type="button"
+            className="text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+          >
+            Back
+          </button>
+        </div>
+      </Link>
       <div className="flex justify-center">
         <img
           src="/image/logo.webp"
-          className="max-h-[80px] m-[20px] mt-[20px]"
+          className="max-h-[80px] m-2"
           alt="Pokedex_Logo"
         />
       </div>
       <div className="w-[90%] max-w-[600px]">
         {pokemon.data && (
-          // <div className="rounded-[20px] overflow-hidden shadow dark:bg-gray-800 dark:border-gray-700 mb-5 p-[50px]">
           <div className="">
-            {/* <div className="bg-center aspect-square w-full bg-cover rounded-[20px]"> */}
-            <div className="">
+            <div className="flex justify-center">
               <img
-                className="animate-tada rounded-t-lg h-[350px] p-[0px] w-full"
+                className="animate-tada rounded-t-lg h-[300px] p-[0px] w-[300px]"
                 src={pokemon.data.image}
-                alt=""
+                alt="Pokemon_Image"
               />
             </div>
             <div className="p-10">
-            <div className="text-2xl text-center">
-              {pokemon.data.types.map((item) => {
-                return (
-                  <span
-                    className={`badge-type-${item.type.name} px-[14px] capitalize py-1 ml-5 rounded-[16px]`}
-                  >
-                    {" "}
-                    {item.type.name}{" "}
-                  </span>
-                );
-              })}
-            </div>
-            <div className="pt-5">
-              <div className="text-center">
-                <h5 className="mb-2 text-5xl font-bold tracking-tight text-white dark:text-white">
-                  #{pokemon.data.id}
-                </h5>
-                <h5 className="mb-2 text-4xl font-bold tracking-tight text-white dark:text-white">
-                  {pokemon.data.name}
-                </h5>
+              <div className="text-2xl text-center">
+                {pokemon.data.types.map((item) => {
+                  return (
+                    <span
+                      className={`badge-type-${item.type.name} px-[14px] capitalize py-1 ml-5 rounded-[16px]`}
+                    >
+                      {" "}
+                      {item.type.name}{" "}
+                    </span>
+                  );
+                })}
+              </div>
+              <div className="pt-5">
+                <div className="text-center">
+                  <h5 className="mb-2 text-5xl font-bold tracking-tight text-white dark:text-white">
+                    #{pokemon.data.id}
+                  </h5>
+                  <h5 className="mb-2 text-4xl font-bold tracking-tight text-white dark:text-white">
+                    {pokemon.data.name}
+                  </h5>
+                </div>
               </div>
             </div>
-          </div>
           </div>
         )}
       </div>
